799cc3060d0be326ca5e5c7f3073338405f321c6
Simon Jockers <449739+sjockers@users.noreply.github.com>
2019-08-30 14:01:39 +0200

    Remove references to Google Fonts (no longer needed)

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -16,10 +16,6 @@ class MyDocument extends Document {
           />
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-          />
         </Head>
         <body>
           <Main />
