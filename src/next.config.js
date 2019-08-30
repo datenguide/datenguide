0583b87c6af810d9c08e2cdcf05230b8c9e158a6
Simon Jockers <449739+sjockers@users.noreply.github.com>
2019-08-30 14:06:19 +0200

    Render static pages from MDX

--- src/next.config.js
+++ src/next.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const withFonts = require('next-fonts')
 const withMDX = require('@zeit/next-mdx')({
-  extension: /\.md?$/
+  extension: /\.mdx?$/
 })
 
 module.exports = withMDX(
@@ -9,7 +9,7 @@ module.exports = withMDX(
     pageExtensions: ['js', 'jsx', 'mdx', 'md'],
     webpack(config, options) {
       config.module.rules.push({
-        test: /\.md?$/,
+        test: /\.mdx?$/,
         use: [path.join(__dirname, './lib/frontmatter-loader')]
       })
       return config
