47b7ad0cb27e9a65b528fc6520a758b0c4acadf8
Christian Rijke <hello@christianrijke.de>
2019-11-03 21:35:09 +0100

    Memoize actions

--- src/pages/statistics/useSearchManager.js
+++ src/pages/statistics/useSearchManager.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useMemo } from 'react'
 import { useManualQuery } from 'graphql-hooks'
 import Router from 'next/router'
 import { camelizeKeys } from 'humps'
@@ -60,122 +60,130 @@ const useSearchManager = (initialQuery, initialMeasures, initialRegions) => {
   const [fetchSchema] = useManualQuery(SCHEMA_QUERY)
   const [fetchRegion] = useManualQuery(REGION_QUERY)
 
-  const asyncActions = {
-    syncUrl: () => async (dispatch, getState) => {
-      const newQuery = {
-        region: Object.values(getState().regions)
-          .map(r => r.id)
-          .join(','),
-        data: Object.values(getState().measures).map(m => m.id)
-      }
-      Router.push({
-        pathname: '/statistics',
-        query: newQuery
-      })
-    },
-    loadMeasure: id => async dispatch => {
-      const [statisticId, measureId] = id.split(':')
-      dispatch(actions.setLoading())
-      const schema = await fetchSchema({
-        variables: {
-          measures: [{ statisticId, measureId }]
+  const asyncActions = useMemo(
+    () => ({
+      syncUrl: () => async (dispatch, getState) => {
+        const newQuery = {
+          region: Object.values(getState().regions)
+            .map(r => r.id)
+            .join(','),
+          data: Object.values(getState().measures).map(m => m.id)
         }
-      })
-      if (schema.error) {
-        dispatch(actions.setError(JSON.stringify(schema.error))) // TODO better error handling
-      } else {
-        dispatch(actions.addMeasure(schema.data.measures[0]))
+        Router.push({
+          pathname: '/statistics',
+          query: newQuery
+        })
+      },
+      loadMeasure: id => async dispatch => {
+        const [statisticId, measureId] = id.split(':')
+        dispatch(actions.setLoading())
+        const schema = await fetchSchema({
+          variables: {
+            measures: [{ statisticId, measureId }]
+          }
+        })
+        if (schema.error) {
+          dispatch(actions.setError(JSON.stringify(schema.error))) // TODO better error handling
+        } else {
+          dispatch(actions.addMeasure(schema.data.measures[0]))
+          dispatch(actions.syncUrl())
+        }
+      },
+      loadRegion: id => async dispatch => {
+        dispatch(actions.setLoading())
+        const region = await fetchRegion({
+          variables: {
+            id
+          }
+        })
+        if (region.error) {
+          dispatch(actions.setError(JSON.stringify(region.error))) // TODO better error handling
+        } else {
+          dispatch(actions.addRegion(region.data.region))
+          dispatch(actions.syncUrl())
+        }
+      },
+      closeRegion: id => async dispatch => {
+        dispatch(actions.removeRegion(id))
+        dispatch(actions.syncUrl())
+      },
+      closeMeasure: id => async dispatch => {
+        dispatch(actions.removeMeasure(id))
+        dispatch(actions.syncUrl())
+      },
+      changeDimensionSelection: payload => async dispatch => {
+        dispatch(actions.updateDimension(payload))
         dispatch(actions.syncUrl())
       }
-    },
-    loadRegion: id => async dispatch => {
-      dispatch(actions.setLoading())
-      const region = await fetchRegion({
-        variables: {
-          id
-        }
-      })
-      if (region.error) {
-        dispatch(actions.setError(JSON.stringify(region.error))) // TODO better error handling
-      } else {
-        dispatch(actions.addRegion(region.data.region))
-        dispatch(actions.syncUrl())
-      }
-    },
-    closeRegion: id => async dispatch => {
-      dispatch(actions.removeRegion(id))
-      dispatch(actions.syncUrl())
-    },
-    closeMeasure: id => async dispatch => {
-      dispatch(actions.removeMeasure(id))
-      dispatch(actions.syncUrl())
-    },
-    changeDimensionSelection: payload => async dispatch => {
-      dispatch(actions.updateDimension(payload))
-      dispatch(actions.syncUrl())
-    }
-  }
+    }),
+    []
+  )
 
-  const reducers = {
-    setLoading: state => {
-      state.loading = true
-      return state
-    },
-    initializeMeasures: (state, action) => {
-      state.measures = measureSchemaListToState(action.payload)
-      state.loading = false
-      return state
-    },
-    initializeRegions: (state, action) => {
-      // TODO transform to state object
-      state.regions = action.payload
-      state.loading = false
-      return state
-    },
-    addRegion: (state, action) => {
-      // TODO transform to state object
-      if (state.regions[action.payload.id]) {
-        state.error = 'Region wurde bereits ausgewählt'
-      } else {
-        state.regions[action.payload.id] = action.payload
+  const reducers = useMemo(
+    () => ({
+      setLoading: state => {
+        state.loading = true
+        return state
+      },
+      initializeMeasures: (state, action) => {
+        state.measures = measureSchemaListToState(action.payload)
         state.loading = false
+        return state
+      },
+      initializeRegions: (state, action) => {
+        // TODO transform to state object
+        state.regions = action.payload
+        state.loading = false
+        return state
+      },
+      addRegion: (state, action) => {
+        // TODO transform to state object
+        if (state.regions[action.payload.id]) {
+          state.error = 'Region wurde bereits ausgewählt'
+        } else {
+          state.regions[action.payload.id] = action.payload
+          state.loading = false
+        }
+        return state
+      },
+      removeRegion: (state, action) => {
+        delete state.regions[action.payload]
+        return state
+      },
+      addMeasure: (state, action) => {
+        if (state.measures[action.payload.id]) {
+          state.error = 'Statistik wurde bereits ausgewählt'
+        } else {
+          state.measures[action.payload.id] = measureSchemaToState(
+            action.payload
+          )
+        }
+        state.loading = false
+        return state
+      },
+      removeMeasure: (state, action) => {
+        delete state.measures[action.payload]
+        return state
+      },
+      updateDimension: (state, action) => {
+        const { id, argCode, diff } = action.payload
+        state.measures[id].dimensions = state.measures[id].dimensions.map(dim =>
+          dim.name === argCode
+            ? {
+                ...dim,
+                ...diff
+              }
+            : dim
+        )
+        return state
+      },
+      setError: (state, action) => {
+        state.error = action.payload
+        return state
       }
-      return state
-    },
-    removeRegion: (state, action) => {
-      delete state.regions[action.payload]
-      return state
-    },
-    addMeasure: (state, action) => {
-      if (state.measures[action.payload.id]) {
-        state.error = 'Statistik wurde bereits ausgewählt'
-      } else {
-        state.measures[action.payload.id] = measureSchemaToState(action.payload)
-      }
-      state.loading = false
-      return state
-    },
-    removeMeasure: (state, action) => {
-      delete state.measures[action.payload]
-      return state
-    },
-    updateDimension: (state, action) => {
-      const { id, argCode, diff } = action.payload
-      state.measures[id].dimensions = state.measures[id].dimensions.map(dim =>
-        dim.name === argCode
-          ? {
-              ...dim,
-              ...diff
-            }
-          : dim
-      )
-      return state
-    },
-    setError: (state, action) => {
-      state.error = action.payload
-      return state
-    }
-  }
+    }),
+    []
+  )
 
   const initialState = {
     measures: {},
