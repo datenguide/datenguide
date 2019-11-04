c24ce7256fc803c20553149a175c34dc3f21bde2
Simon Jockers <449739+sjockers@users.noreply.github.com>
2019-11-04 14:20:38 +0100

    Meta: Add default social media preview image

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,22 +1,28 @@
 import React from 'react'
 import Head from 'next/head'
 
-const Meta = ({ title, description, previewImage }) => (
-  <Head>
-    <title>{title}</title>
-    <meta name="description" content={description} />
-    <meta property="og:type" content="website" />
-    <meta name="og:title" property="og:title" content={title} />
-    <meta
-      name="og:description"
-      property="og:description"
-      content={description}
-    />
-    <meta name="twitter:card" content="summary" />
-    <meta name="twitter:title" content={title} />
-    <meta name="twitter:description" content={description} />
-    <meta property="og:image" content={previewImage} />
-    <meta name="twitter:image" content={previewImage} />
-  </Head>
-)
+const BASE_URL = 'https://datengui.de'
+const DEFAULT_IMAGE = 'social/default.png'
+
+const Meta = ({ title, description, previewImage }) => {
+  const imagePath = `${BASE_URL}/${previewImage || DEFAULT_IMAGE}`
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta property="og:type" content="website" />
+      <meta name="og:title" property="og:title" content={title} />
+      <meta
+        name="og:description"
+        property="og:description"
+        content={description}
+      />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta property="og:image" content={imagePath} />
+      <meta name="twitter:image" content={imagePath} />
+    </Head>
+  )
+}
 export default Meta
