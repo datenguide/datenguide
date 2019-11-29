2abb97b97e717c09609a607748c29cadf6ef6964
Christian Rijke <hello@christianrijke.de>
2019-11-29 14:22:52 +0100

    Lint

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -4,8 +4,6 @@ import { makeStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
-import Checkbox from '@material-ui/core/Checkbox'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
 
 import background from '../assets/hero_city.svg'
 
