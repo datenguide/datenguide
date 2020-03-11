a63b79243004654f646a07c01cc9877e0446125b
Christian Rijke <hello@christianrijke.de>
2020-03-11 19:33:02 +0100

    Render workaround link on both client and server.

--- src/components/HeaderToolbar.js
+++ src/components/HeaderToolbar.js
@@ -66,7 +66,7 @@ export default function HeaderToolbar({ menuButton }) {
         </Link>
       </div>
       <div className={classes.controls}>
-        {!process.browser && process.env.NODE_ENV !== 'production' ? (
+        {!process.env.NODE_ENV !== 'production' ? (
           <a
             className="MuiButtonBase-root MuiButton-root MuiButton-text MuiButton-colorInherit"
             href="/docs/gettingstarted/intro"
