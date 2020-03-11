4978fdc151cb4722a62c887f30866a9d99eb8723
Christian Rijke <hello@christianrijke.de>
2020-03-11 22:26:30 +0100

    Replace custom mdx loader with mdx-enhanced and custom toc builder.

--- src/next.config.js
+++ src/next.config.js
@@ -2,13 +2,33 @@ const path = require('path')
 const withFonts = require('next-fonts')
 const withCSS = require('@zeit/next-css')
 const withImages = require('next-images')
-const withMDX = require('./lib/mdx/withMdx')
+const withMdxEnhanced = require('next-mdx-enhanced')
+
+const slug = require('remark-slug')
+const images = require('remark-images')
+const normalizeHeadings = require('remark-normalize-headings')
+const rehypePrism = require('@mapbox/rehype-prism')
+const generateToc = require('./lib/mdx/generateToc')
 const Dotenv = require('dotenv-webpack')
 
 require('dotenv').config()
 
 module.exports = withCSS(
-  withMDX()(
+  withMdxEnhanced({
+    layoutPath: 'layouts',
+    defaultLayout: false,
+    fileExtensions: ['mdx'],
+    remarkPlugins: [slug, images, normalizeHeadings],
+    rehypePlugins: [rehypePrism],
+    extendFrontMatter: {
+      process: mdxContent => {
+        return {
+          tableOfContents: generateToc(mdxContent)
+        }
+      },
+      phase: 'both'
+    }
+  })(
     withImages(
       withFonts({
         pageExtensions: ['js', 'jsx', 'mdx', 'md'],
