4f17a244ec8ae9eb3c771be3ae53ac8fe55bd6d0
Simon Jockers <449739+sjockers@users.noreply.github.com>
2020-03-30 17:55:12 +0200

    Apply correct layout to Baden-Württemberg region page

--- src/pages/region/baden-wuerttemberg.js
+++ src/pages/region/baden-wuerttemberg.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import Grid from '@material-ui/core/Grid'
+import { Container, Grid } from '@material-ui/core'
 import { useQuery } from 'graphql-hooks'
-import DefaultLayout from '../../layouts/Default'
+import DefaultLayout from '../../layouts/DefaultLayout'
 import FigureHighlight from '../../components/regions/FigureHighlight'
 import MapCities from '../../components/regions/MapCities'
 
@@ -39,37 +39,40 @@ const RegionBadenWuerttemberg = () => {
 
   return (
     <DefaultLayout meta={{ title: name }}>
-      <Grid container>
-        <Grid item xs={4}>
-          <h1>{name}</h1>
-          <p>
-            Baden-Württemberg ist nach Bayern und Nordrhein-Westfalen das
-            drittgrößte Bundesland Deutschlands, sowohl bei der Fläche als auch
-            bei der Einwohnerzahl. Die größte Stadt in Baden-Württemberg ist die
-            Landeshauptstadt Stuttgart, gefolgt von Karlsruhe und Heidelberg.
-          </p>
+      <Container>
+        <Grid container>
+          <Grid item xs={4}>
+            <h1>{name}</h1>
+            <p>
+              Baden-Württemberg ist nach Bayern und Nordrhein-Westfalen das
+              drittgrößte Bundesland Deutschlands, sowohl bei der Fläche als
+              auch bei der Einwohnerzahl. Die größte Stadt in Baden-Württemberg
+              ist die Landeshauptstadt Stuttgart, gefolgt von Karlsruhe und
+              Heidelberg.
+            </p>
 
-          <FigureHighlight />
+            <FigureHighlight />
+          </Grid>
+          <Grid item xs={8}>
+            <div className={classes.primaryVis}>
+              <MapCities />
+            </div>
+          </Grid>
         </Grid>
-        <Grid item xs={8}>
-          <div className={classes.primaryVis}>
-            <MapCities />
-          </div>
-        </Grid>
-      </Grid>
 
-      <h2>Bevölkerungsdichte</h2>
+        <h2>Wahlergebnisse</h2>
 
-      <table>
-        <tbody>
-          {WAHL09.map(({ PART04: name, value }) => (
-            <tr key={name}>
-              <th>{name}</th>
-              <td>{value} votes</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+        <table>
+          <tbody>
+            {WAHL09.map(({ PART04: name, value }) => (
+              <tr key={name}>
+                <th>{name}</th>
+                <td>{value} votes</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Container>
     </DefaultLayout>
   )
 }
