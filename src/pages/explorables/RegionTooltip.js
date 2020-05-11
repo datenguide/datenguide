c8be7dcb0b6ff40bebd458e40687b5c389e92d3f
Simon Jockers <449739+sjockers@users.noreply.github.com>
2020-05-11 21:39:13 +0200

    Add styling and demo content for region tooltip in ScrollyMap

--- src/pages/explorables/RegionTooltip.js
+++ src/pages/explorables/RegionTooltip.js
@@ -10,6 +10,33 @@ const useStyles = makeStyles((theme) => ({
   root: {
     width: 240,
   },
+
+  title: {
+    margin: 0,
+  },
+
+  hed: {
+    fontSize: 14,
+    marginTop: 0,
+    marginBottom: 10,
+  },
+
+  image: {
+    width: '260px',
+    position: 'relative',
+    left: '-10px',
+  },
+
+  facts: {
+    fontSize: 14,
+    listStyle: 'none',
+    margin: 0,
+    padding: 0,
+  },
+
+  highlight: {
+    fontSize: '1.3em',
+  },
 }))
 
 export default function RegionTooltip({ title, lonLat }) {
@@ -18,7 +45,29 @@ export default function RegionTooltip({ title, lonLat }) {
   return (
     <MapTooltip lonLat={lonLat} offsetTop={-15}>
       <div className={classes.root}>
-        <h3>{title}</h3>
+        <h3 className={classes.title}>{title}</h3>
+        <p className={classes.hed}>Gemeinde in Nordrhein-Westfalen</p>
+        <img className={classes.image} src="/altena.jpg" width="100%" />
+        <ul className={classes.facts}>
+          <li>
+            <b className={classes.highlight}>16.922</b>
+            <span>Einwohner</span>
+            <span>(2018)</span>
+          </li>
+          <li>
+            Bevölkerungsdichte:{' '}
+            <b>
+              381 Einwohner je km<sup>2</sup>
+            </b>
+          </li>
+          <li>
+            Fläche:{' '}
+            <b>
+              44,42 km<sup>2</sup>
+            </b>
+          </li>
+          <a href="/region/maerkischer-kreis">Alle Daten zu Altena</a>
+        </ul>
       </div>
     </MapTooltip>
   )
