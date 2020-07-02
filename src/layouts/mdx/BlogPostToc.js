0595cb44e4d425b5e59bd0399cbcac53081a82c7
P3nny <p3nny@hacking.studio>
2020-07-02 13:47:15 +0200

    🚧  First try BlogPostToc

---
+++ src/layouts/mdx/BlogPostToc.js
@@ -0,0 +1,50 @@
+import moment from 'moment'
+
+import Container from '@material-ui/core/Container'
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+
+import Base from '../Base'
+import HeaderToolbar from '../../components/HeaderToolbar'
+import Footer from '../../components/Footer'
+import BlogHeader from '../../components/BlogHeader'
+
+const useStyles = makeStyles((theme) => ({
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(2, 0, 30, 0),
+  },
+  metaInfo: {
+    color: theme.palette.grey[500],
+    fontWeight: 'bold',
+  },
+}))
+
+const BlogPostToc = (frontMatter) => ({ children, tableOfContents }) => {
+  const classes = useStyles()
+
+  return (
+    <Base meta={frontMatter}>
+      <AppBar position="fixed">
+        <HeaderToolbar />
+      </AppBar>
+      <BlogHeader
+        title={frontMatter.title}
+        description={frontMatter.description}
+      />
+      <div className={classes.content}>
+        <Container maxWidth="lg">
+          <div className={classes.metaInfo}>
+            {moment(frontMatter.date).format('DD.MM.YYYY')} -{' '}
+            {frontMatter.author}
+          </div>
+        </Container>
+        <div>{tableOfContents}</div>
+        {children}
+      </div>
+      <Footer />
+    </Base>
+  )
+}
+
+export default BlogPostToc
