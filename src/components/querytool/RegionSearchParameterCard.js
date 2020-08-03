b63c7fafe49ae26b00f058fc45393595163c4ea3
Simon Jockers <449739+sjockers@users.noreply.github.com>
2020-08-03 15:53:59 +0200

    Add basic select element for regional level

--- src/components/querytool/RegionSearchParameterCard.js
+++ src/components/querytool/RegionSearchParameterCard.js
@@ -1,12 +1,16 @@
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 import { makeStyles } from '@material-ui/core/styles'
 import Accordion from '@material-ui/core/ExpansionPanel' // TODO rename to accordion after material-ui update
 import AccordionSummary from '@material-ui/core/ExpansionPanelSummary' // TODO rename to accordion after material-ui update
 import AccordionDetails from '@material-ui/core/ExpansionPanelDetails' // TODO rename to accordion after material-ui update
+import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
 import CloseIcon from '@material-ui/icons/Close'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
 
 const useStyles = makeStyles((theme) => ({
   root: {},
@@ -25,16 +29,44 @@ const useStyles = makeStyles((theme) => ({
     fontWeight: theme.typography.subtitle2.fontSize,
     color: theme.palette.grey[500],
   },
+  regionTitle: {
+    fontWeight: 'bold',
+  },
+  menuItem: {},
 }))
 
-const nutsToText = {
-  1: 'NUTS 1 – Bundesland',
-  2: 'NUTS 2 – Regierungsbezirk',
-  3: 'NUTS 3 – Landkreis, Kreis, kreisfreie Stadt',
-}
+const regionLevels = [
+  { level: 1, title: 'NUTS 1', description: 'Bundesländer' },
+  {
+    level: 2,
+    title: 'NUTS 2',
+    description: 'Regierungsbezirke und statistische Regionen',
+  },
+  {
+    level: 3,
+    title: 'NUTS 3',
+    description: 'Landkreise und kreisfreie Städte',
+  },
+]
 
 const RegionSearchParameterCard = ({ region, onClose }) => {
   const classes = useStyles()
+  const [menuAnchor, setMenuAnchor] = useState(null)
+
+  const handleMenuOpen = (event) => {
+    event.stopPropagation()
+    setMenuAnchor(event.currentTarget)
+  }
+
+  const handleMenuClose = () => {
+    setMenuAnchor(null)
+  }
+
+  const handleMenuSelect = (event, level) => {
+    event.stopPropagation()
+    setMenuAnchor(null)
+    console.log('menu select!', level) // eslint-disable-line
+  }
 
   return (
     <Accordion className={classes.root}>
@@ -42,7 +74,35 @@ const RegionSearchParameterCard = ({ region, onClose }) => {
         <div className={classes.summary}>
           <div className={classes.headingAttribute}>{region.name}</div>
           <div className={classes.headingStatistic}>
-            {nutsToText[region.level]}
+            <Button
+              aria-controls="simple-menu"
+              aria-haspopup="true"
+              onClick={handleMenuOpen}
+            >
+              <div className={classes.regionTitle}>
+                {region.level && regionLevels[region.level - 1].description}
+              </div>
+            </Button>
+            <Menu
+              id="region-menu"
+              anchorEl={menuAnchor}
+              keepMounted
+              open={Boolean(menuAnchor)}
+              onClose={handleMenuClose}
+            >
+              {regionLevels.map(({ level, title, description }) => {
+                return (
+                  <MenuItem
+                    key={level}
+                    className={classes.menuItem}
+                    selected={region.level === level}
+                    onClick={(event) => handleMenuSelect(event, level)}
+                  >
+                    {title} – {description}
+                  </MenuItem>
+                )
+              })}
+            </Menu>
           </div>
         </div>
         <IconButton
