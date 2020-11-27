abe8469ab3c5906752f498f1cff29e80c9801732
Simon Jockers <449739+sjockers@users.noreply.github.com>
2020-11-28 00:22:23 +0100

    Add margin to Datawrapper chart embeds

--- src/components/DatawrapperChart.js
+++ src/components/DatawrapperChart.js
@@ -1,7 +1,18 @@
 import React from 'react'
-
 import DWChart from 'react-datawrapper-chart'
+import { makeStyles } from '@material-ui/core/styles'
 
-export default ({ ...props }) => {
-  return <DWChart {...props} />
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: theme.spacing(2, 0),
+  },
+}))
+
+export default function BaseLayout(props) {
+  const classes = useStyles()
+  return (
+    <div className={classes.root}>
+      <DWChart {...props} />
+    </div>
+  )
 }
