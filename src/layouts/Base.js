2b49d80ab9d440e6a85d12a3b18b513800bc6d8b
Simon Jockers <449739+sjockers@users.noreply.github.com>
2020-11-28 00:20:57 +0100

    Add styling for footnotes

--- src/layouts/Base.js
+++ src/layouts/Base.js
@@ -12,7 +12,45 @@ const useStyles = makeStyles((theme) => ({
     [theme.breakpoints.down('sm')]: {
       fontSize: theme.typography.body2.fontSize,
     },
+
+    '& .footnotes': {
+      padding: theme.spacing(3, 0),
+
+      '& hr': {
+        border: 0,
+        height: 1,
+        background: theme.palette.grey[400],
+        outline: 0,
+      },
+
+      '& a': {
+        color: theme.palette.grey[600],
+      },
+
+      '& ol': {
+        color: theme.palette.grey[600],
+        padding: theme.spacing(4, 3),
+        fontSize: theme.typography.body2.fontSize,
+      },
+
+      '& li': {
+        marginBottom: theme.spacing(2),
+      },
+    },
+
+    '& .footnote-backref': {
+      textDecoration: 'none',
+      marginLeft: theme.spacing(1),
+      verticalAlign: 'sub',
+      fontFamily: 'ibm-plex-sans',
+    },
+
+    '& .footnote-ref': {
+      textDecoration: 'none',
+      fontSize: theme.typography.body2.fontSize,
+    },
   },
+
   toolbarSpacing: theme.mixins.toolbar,
 }))
 
