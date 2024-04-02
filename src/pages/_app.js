a5482ececfeee893c6b686d6d98e3997731295fe
Christian Rijke <hello@christianrijke.de>
2024-04-02 22:41:45 +0200

    Remove graphql, query tool, datenguide API requests.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,30 +1,26 @@
 import App from 'next/app'
 
 import { StateInspector } from 'reinspect'
-import { ClientContext } from 'graphql-hooks'
 
 import { ThemeProvider } from '@material-ui/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 
-import withGraphQLClient from '../lib/withGraphQLClient'
 import theme from '../theme'
 
 import '../lib/vendor/prism-material-dark.css'
 
 class _App extends App {
   render() {
-    const { Component, pageProps, graphQLClient } = this.props
+    const { Component, pageProps } = this.props
     return (
       <StateInspector name="Datenguide">
-        <ClientContext.Provider value={graphQLClient}>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Component {...pageProps} />
-          </ThemeProvider>
-        </ClientContext.Provider>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
       </StateInspector>
     )
   }
 }
 
-export default withGraphQLClient(_App)
+export default _App
