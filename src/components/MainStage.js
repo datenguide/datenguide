329971baa6c0d23b0f245652e6ccadfe664369b0
Simon Jockers <449739+sjockers@users.noreply.github.com>
2024-06-15 20:05:51 +0200

    Update description on the home and about pages

--- src/components/MainStage.js
+++ src/components/MainStage.js
@@ -8,8 +8,8 @@ const useStyles = makeStyles((theme) => ({
   root: {
     minHeight: '480px',
     backgroundColor: '#c3e5f1',
-    paddingTop: theme.spacing(3),
-    paddingBottom: theme.spacing(6),
+    paddingTop: theme.spacing(5),
+    paddingBottom: theme.spacing(5),
     borderBottom: '1px solid',
     fontSize: theme.typography.body1.fontSize,
 
